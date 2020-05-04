f9d3ce41fb8727c7d9f047e41c0416aa8c34cb24
ChristopherPence <chris1599@live.com>
2020-05-04 14:06:52 -0400

    database setup code

--- code/schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE test(
-    a VARCHAR(15),
-    b INT
-);
---
+++ code/setup-db.sql
@@ -0,0 +1,3 @@
+CREATE USER final_project_user WITH PASSWORD 'whydidgaryleaveus';
+CREATE DATABASE final_project;
+GRANT ALL PRIVILEGES ON DATABASE final_project TO final_project_user;

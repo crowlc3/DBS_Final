68ee352831f4f0aff6c4fcb7a6d46aa4b5e090fb
crowlc3 <crowlc3@rpi.edu>
2020-05-04 19:11:47 -0400

    Some starting ideas for queries

--- code/application.py
+++ code/application.py
@@ -6,3 +6,38 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+
+#-------------Heres some ideas for queries-------------------#
+
+
+
+# - At least two queries that select at least some data from both of your datasets
+
+#*****Toxins*****
+# Select the toxins levels per country
+# Select the amount of a specific toxin in all counties
+# Select the amount of toxins for a specific county
+
+#*****Cancers*****
+# Select the data from each type of cancer
+# Select the data for a specific county
+# Select the data with cases over a certain threshold
+
+
+# - At least two queries that showcase syntax beyond the basic `SELECT-FROM-WHERE` clauses (e.g., Grouping, Subqueries, etc.)
+
+#*****Toxins*****
+#
+
+#*****Cancers*****
+#
+
+
+# - At least two queries that accept input entered by the user (as opposed to just allowing selection from a list of options)
+
+#*****Toxins*****
+#
+
+#*****Cancers*****
+#

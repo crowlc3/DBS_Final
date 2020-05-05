b2fbd86d0ad9da8153063a8b086eaa5e13f3e141
crowlc3 <crowlc3@rpi.edu>
2020-05-04 21:29:51 -0400

    basic framework

--- code/application.py
+++ code/application.py
@@ -1,12 +1,17 @@
-import os
-
-def main():
-	print("Welcome to our application!");
+def test():
+	return
 
 
 if __name__ == "__main__":
-	main()
-
+	print("Welcome to our application")
+	print("Contributors: Christopher Pence, Howard Zhao, Aidan Duane, Caitlin Crowley")
+	print("Please pick from one of these options to query our database:")
+	arr = ["1: print all of the ____" , "2: print all of the _______" , "3: do this _____"]
+	for item in arr:
+		print(item)
+	query = input("Enter the number of the query you would like to run: ")
+	query_ = int(query)
+	print("You selected",arr[query_-1])
 
 #-------------Heres some ideas for queries-------------------#
 
